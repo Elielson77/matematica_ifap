7c23b271fba52b9b4ac279b3434e4843687ecd7d
Elielson77 <elielsonll77@gmail.com>
2022-05-31 00:09:30 -0300

    commit inicial com rotas de páginas

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const select = document.querySelector('#select')
+const submit = document.querySelector('#submit')
+let form = document.querySelector('#form')
+let valor;
+
+submit.addEventListener('click', (e) => {
+  select.addEventListener('change', () => { valor = select.value; })
+  changePage()
+  console.log(form.action)
+})
+
+function changePage() {
+  if (valor === 'Probabilidade') return form.action = './pages/probabilidade/index.html';
+  if (valor === 'Arranjo') return form.action = './pages/arranjo/index.html';
+  if (valor === 'Combinação') return form.action = './pages/combinacao/index.html';
+  if (valor === 'Permutação') return form.action = './pages/permutacao/index.html';
+}
+
