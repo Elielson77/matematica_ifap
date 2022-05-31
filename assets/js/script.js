d66e0d57dc4732a7581a9ae88fda7248aa76c0b2
Elielson77 <elielsonll77@gmail.com>
2022-05-31 00:17:40 -0300

    deixa sem rota se nao for selecionado nenhum modo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,14 @@ let form = document.querySelector('#form')
 let valor;
 
 submit.addEventListener('click', (e) => {
+  e.preventDefault();
   select.addEventListener('change', () => { valor = select.value; })
   changePage()
   console.log(form.action)
 })
 
 function changePage() {
+  if (valor === '#') return form.action = '#';
   if (valor === 'Probabilidade') return form.action = './pages/probabilidade/index.html';
   if (valor === 'Arranjo') return form.action = './pages/arranjo/index.html';
   if (valor === 'Combinação') return form.action = './pages/combinacao/index.html';
