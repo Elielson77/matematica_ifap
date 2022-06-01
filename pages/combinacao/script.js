5472ced27175ac54b4dac5777a93034c355fb930
Elielson77 <elielsonll77@gmail.com>
2022-06-01 09:54:33 -0300

    adicionado 0! = 1, e lógica de erro na pagina de combinação

--- pages/combinacao/script.js
+++ pages/combinacao/script.js
@@ -6,14 +6,25 @@ const resultado = document.querySelector('#resultado')
 submit.addEventListener('click', (e) => {
   e.preventDefault()
   const divisor = calculaFatorial(elemComb.value) * calculaFatorial(elementos.value - elemComb.value)
+  const resul = calculaFatorial(elementos.value) / divisor;
 
-  resultado.innerHTML = `É possível formar ${calculaFatorial(elementos.value) / divisor} combinações!`
+  if (resul < 0 || !resul) {
+    resultado.innerHTML = 'Tem algo errado com sua conta!'
+    resultado.classList.add('erro')
+  } else {
+    resultado.innerHTML = `É possível formar ${resul} combinações!`
+    resultado.classList.remove('erro')
+  }
+
 })
 
 const calculaFatorial = (n) => {
   let arranjo = n;
   for (let i = 1; i < n; i++) {
     arranjo *= i;
+  }
+  if (n === 0) {
+    return 1;
   }
   return arranjo;
 }
